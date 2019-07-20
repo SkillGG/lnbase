b38a0f408c498c760948bc4ce60d7d09cf20ed68
SkillGG <github@skilletss.cba.pl>
2019-07-20 22:23:25 +0200

    SceneHandler comments

--- Base/SceneHandler.cs
+++ Base/SceneHandler.cs
@@ -18,6 +18,13 @@ namespace lnbase.Base {
 
 		public List<GameScene> allScenes;
 
+		private GameBase Base;
+
+		/// <summary>
+		/// Returns scene with ID as given string
+		/// </summary>
+		/// <param name="s"></param>
+		/// <returns></returns>
 		public GameScene this[string s] {
 			get {
 				if( s == null || s == "" )
@@ -26,12 +33,22 @@ namespace lnbase.Base {
 			}
 		}
 
+		/// <summary>
+		/// Returns first occurence of scene with given Flag
+		/// <para>Used to find FIRST, END and ERROR scenes</para>
+		/// </summary>
+		/// <param name="f"></param>
+		/// <returns></returns>
 		public GameScene this[Flag f] {
 			get {
 				return allScenes.Find((GameScene s) => s.FLAG == f);
 			}
 		}
 
+		/// <summary>
+		/// Switches to given GameScene
+		/// </summary>
+		/// <param name="ngs"></param>
 		public void Next(GameScene ngs) {
 			if( allScenes.Contains(ngs) ) {
 				current = ngs.ID;
@@ -39,11 +56,17 @@ namespace lnbase.Base {
 			}
 		}
 
-		private GameBase Base;
+		/// <summary>
+		/// Terminates whole game
+		/// </summary>
 		public void Terminate() {
 			Base.Quit( );
 		}
 
+		/// <summary>
+		/// Adds new GameScene to list
+		/// </summary>
+		/// <param name="gs"></param>
 		public void Add(GameScene gs) {
 			if( this[gs.ID] != null ) {
 				// error
@@ -54,6 +77,9 @@ namespace lnbase.Base {
 				current = gs.ID;
 		}
 
+		/// <summary>
+		/// Starts showing FIRST scene ( Terminate() if no FIRST Scene present )
+		/// </summary>
 		public void Start() {
 			Next(this[Flag.FIRST] ?? allScenes[0]);
 			if( Current == null )
@@ -69,6 +95,12 @@ namespace lnbase.Base {
 		public SceneBars DefaultBARS { get; private set; }
 		public SpriteFont DefaultFONT { get; private set; }
 
+		/// <summary>
+		/// Sets defaults for Background, Bars and Font
+		/// </summary>
+		/// <param name="defBCKG"></param>
+		/// <param name="defBARS"></param>
+		/// <param name="defFONT"></param>
 		public void SetSceneDefaults(SceneBackground defBCKG, SceneBars defBARS, SpriteFont defFONT) {
 			DefaultBCKG = defBCKG;
 			DefaultBARS = defBARS;
